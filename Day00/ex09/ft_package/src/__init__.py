48e799b81572f42208503ac49efd20ca8bacb951
adrouin <amaury.d.pro@proton.me>
2026-02-13 18:33:36 +0100

    day00 ex09 en cours

---
+++ Day00/ex09/ft_package/src/__init__.py
@@ -0,0 +1,3 @@
+from ft_package.src.count_in_list import count_in_list
+
+__all__ = ['count_in_list']
---
+++ Day00/ex09/ft_package/src/count_in_list.py
@@ -0,0 +1,9 @@
+def count_in_list(lst, item):
+    """
+    Docstring pour count_in_list
+    Count the number of occurences of an item in a list
+    :param lst: The list to search in
+    :param item: The item to count
+    :return The number of times items appears in the list
+    """
+    return lst.count(item)
