cdce9ffa9e0dcd3de6a4e12b956d2f0f1c96a26d
adrouin <amaury.d.pro@proton.me>
2026-02-16 16:21:56 +0100

    Day03 ex01

---
+++ Day03/ex01/S1E7.py
@@ -0,0 +1,31 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Barartheon"
+        self.eyes = "brown"
+        self.hairs = "black"
+    
+    def die(self):
+        self.is_alive = not self.is_alive
+
+
+
+
+
+class Lannister(Character):
+    def __init__(self, first_name, is_alive=True):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+    def die(self):
+        self.is_alive = not self.is_alive
+        
+    @classmethod
+    def create_lannister(cls, first_name, is_alive):
+        instance = cls(first_name)
+        instance.is_alive = is_alive
+        return instance
+        
