c2917e9fce134264859f450b94f9021560131398
adrouin <amaury.d.pro@proton.me>
2026-06-18 10:34:11 +0200

    add: Collen C

---
+++ C/Colleen/Colleen.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+//Outside comment : LET ME IN
+void function(){char *a="#include <stdio.h>%c//Outside comment : LET ME IN%cvoid function(){char *a=%c%s%c;printf(a,10,10,34,a,34,10,10,10);}%cint main(){%c//inside comment : LET ME OUT%cfunction();return 0;}";printf(a,10,10,34,a,34,10,10,10);}
+int main(){
+//inside comment : LET ME OUT
+function();return 0;}
